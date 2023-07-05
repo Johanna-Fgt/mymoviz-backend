295f2a9c9e28fe822dda8c41e2e45aa141202c85
johanna <johannafaget@gmail.com>
2023-07-05 11:40:36 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+const fetch = require('node-fetch');
+
+/* GET - dernières "découvertes" */
+router.get('/movies', (req, res) => {
+	const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
+	const config = {
+		method: 'GET',
+		headers: {
+			accept: 'application/json',
+			Authorization: `Bearer ${process.env.API_KEY}`,
+		},
+	};
+	fetch(url, config)
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data.results[0]);
+			const movies = data.results.map((el) => ({
+				title: el.title,
+				poster: `https://image.tmdb.org/t/p/w500/${el.poster_path}`,
+				voteAverage: el.vote_average,
+				voteCount: el.vote_count,
+				overview:
+					el.overview.split('').length > 250
+						? `${el.overview.slice(0, 250)} ...`
+						: el.overview,
+			}));
+			console.log(movies);
+			data
+				? res.json({ result: true, movies })
+				: res.json({ result: false, error: 'No movies' });
+		});
+});
+
+// Movies list frontend
+// const moviesData = [
+//   { title: 'Forrest Gump', poster: 'forrestgump.jpg', voteAverage: 9.2, voteCount: 22_705, overview: 'A man with a low IQ has accomplished great things in his life and been present during significant historic events—in each case.' },
+//   { title: 'The Dark Knight', poster: 'thedarkknight.jpg', voteAverage: 8.5, voteCount: 27_547, overview: 'Batman raises the stakes in his war on crime and sets out to dismantle the remaining criminal organizations that plague the streets.' },
+//   { title: 'Your name', poster: 'yourname.jpg', voteAverage: 8.5, voteCount: 8_691, overview: 'High schoolers Mitsuha and Taki are complete strangers living separate lives. But one night, they suddenly switch places.' },
+//   { title: 'Iron Man', poster: 'ironman.jpg', voteAverage: 7.6, voteCount: 22_7726, overview: 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.' },
+//   { title: 'Inception', poster: 'inception.jpg', voteAverage: 8.4, voteCount: 31_546, overview: 'Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of his targets is offered a chance to regain his old life.' },
+// ];
+module.exports = router;
